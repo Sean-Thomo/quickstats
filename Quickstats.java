633b73c26f59d566d786b515169ef345ff5a0177
Sean Thomo <seansthomo@gmail.com>
2023-03-03 10:51:21 +0200

    add exception for non integers

--- Quickstats.java
+++ Quickstats.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Quickstats {
     public static void main(String[] args) {
         Stats stat = new Stats(args);
+        stat.getArray();
         System.out.println("Count: "+ stat.count());
         System.out.println("Total: "+ stat.total());
         System.out.println("Average: "+ stat.average());
@@ -15,9 +16,20 @@ class Stats {
     public Stats(String[] args) {
         if (args.length != 0) {
             for(int i = 0; i < args.length; i++) {
-                listArgs.add(Integer.parseInt(args[i]));
+                try {
+                    listArgs.add(Integer.parseInt(args[i]));                    
+                } catch (Exception e) {
+                    System.out.println("You can only input numbers");
+                    break;
+                }
             }
+        } else if (args.length == 0) {
+            System.out.println("There are no numbers in the list");
         }
+    }
+
+    public void getArray() {
+        System.out.println(listArgs);
     }
 
     public int count() {
