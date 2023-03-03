976bc62329523d6caa2b626845b1f84751f27d71
Sean Thomo <seansthomo@gmail.com>
2023-03-03 10:39:43 +0200

    try using OOP principles to create the project

--- Quickstats.java
+++ Quickstats.java
@@ -2,14 +2,43 @@ import java.util.ArrayList;
 
 public class Quickstats {
     public static void main(String[] args) {
-        ArrayList<String> listArgs = new ArrayList<String>();
+        Stats stat = new Stats(args);
+        System.out.println("Count: "+ stat.count());
+        System.out.println("Total: "+ stat.total());
+        System.out.println("Average: "+ stat.average());
+    }
+}
 
+class Stats {
+    private ArrayList<Integer> listArgs = new ArrayList<Integer>();
+
+    public Stats(String[] args) {
         if (args.length != 0) {
-            for(String arg: args) {
-                listArgs.add(arg);
+            for(int i = 0; i < args.length; i++) {
+                listArgs.add(i);
             }
         }
+    }
 
-        System.out.println(listArgs);
+    public int count() {
+        int count = listArgs.size();
+        return count;
+    }
+
+    public int total() {
+        int sum = 0;
+
+        for(int num: listArgs) {
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    public int average() {
+        int average = 0;
+        int total = total();
+        int count = count();
+        return total / count;
     }
 }
